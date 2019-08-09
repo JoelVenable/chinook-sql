b2798876d4d25f2fed822c218248485a8616b6ab
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-08-09 10:22:29 -0500

    Added query for invoice 37 items

---
+++ invoice_37_line_item_count.sql
@@ -0,0 +1,16 @@
+-- Looking at the InvoiceLine table, provide a query
+-- that COUNTs the number of line items for Invoice ID 37.
+
+
+USE Chinook
+
+SELECT i.InvoiceId, COUNT(i.InvoiceId) AS NumItems
+FROM InvoiceLine i
+WHERE i.InvoiceId = 37
+
+
+--  More generic table...
+
+SELECT i.InvoiceId, COUNT(i.InvoiceId) AS NumItems
+FROM InvoiceLine i
+GROUP BY i.InvoiceId
